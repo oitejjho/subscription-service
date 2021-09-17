67b9d74967711b4e084ba827f26ac5fa2643cfd1
Oitejjho Dutta <oitejjhodutta@AGC-FVFD703AP3YV.local>
2021-09-17 13:55:35 +0700

    in mem db added.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-CREATE TABLE if not exists `subscribed_user` (
+/*
+CREATE TABLE `subscribed_user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `subscription_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
@@ -10,4 +11,33 @@ CREATE TABLE if not exists `subscribed_user` (
   `active_flag` boolean NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;*/
+
+DROP TABLE IF EXISTS subscribed_user;
+
+--CREATE TABLE subscribed_user (
+--  id INT AUTO_INCREMENT  PRIMARY KEY,
+--  first_name VARCHAR(250) NOT NULL,
+--  last_name VARCHAR(250) NOT NULL,
+--  career VARCHAR(250) DEFAULT NULL
+--);
+
+
+CREATE TABLE subscribed_user (
+  `id` INT unsigned NOT NULL AUTO_INCREMENT,
+  `subscription_id` varchar(255) NOT NULL UNIQUE,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `first_name` varchar(100) NULL,
+  `gender` varchar(55) NULL,
+  `date_of_birth` char(10) NOT NULL,
+  `consent_flag` boolean NOT NULL,
+  `newsletter_id` varchar(255) NOT NULL,
+  `active_flag` boolean NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+--INSERT INTO billionaires (first_name, last_name, career) VALUES
+--  ('Aliko', 'Dangote', 'Billionaire Industrialist'),
+--  ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
+--  ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
